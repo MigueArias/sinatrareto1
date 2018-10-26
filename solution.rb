409fce3a2f2203d787e418adfe714a8b210fe2ab
Migue Arias <miguearias@gmail.com>
2018-10-25 19:38:15 -0500

    Other Bug

--- solution.rb
+++ solution.rb
@@ -2,5 +2,11 @@ require 'sinatra'
 
 get "/" do
     @name = params["name"]
+    if params.length <= 0 
+        @name = "Persona desconocida"
+    end 
+    if @name == "" 
+        @name = "Persona desconocida"
+    end 
     erb :index
 end
